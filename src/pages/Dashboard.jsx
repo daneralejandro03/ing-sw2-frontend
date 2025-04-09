005588f8fa75887fb9fe21940fd4cdce70a3a8e5
JaimeCardona06 <jaime.cardona40549@ucaldas.edu.co>
2025-04-09 00:43:35 -0500

    feat: the user's name was added to the dasboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,9 +2,10 @@
 import React from "react";
 
 const Dashboard = () => {
+    const user = JSON.parse(localStorage.getItem("user"))
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>Bienvenido al Dashboard</h1>
+      <h1>Bienvenido al Dashboard {user?.fullname}</h1>
       <p>Has iniciado sesión correctamente</p>
     </div>
   );
