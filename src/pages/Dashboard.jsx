4a1606cc7910500b225a608b07b2a6253e560df3
Daner Alejandro Salazar Colorado <daneralejandro03@gmail.com>
2025-04-09 08:07:43 -0500

    Your commit message describing the changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,14 @@
 // src/pages/Dashboard.jsx
 import React from "react";
+import UploadCsvButton from "../components/uploadCsv/buttonUpload"; // Ajusta la ruta si es necesario
 
 const Dashboard = () => {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Bienvenido al Dashboard</h1>
       <p>Has iniciado sesión correctamente</p>
+      {/* Aquí se renderiza el botón para subir el archivo CSV */}
+      <UploadCsvButton />
     </div>
   );
 };
